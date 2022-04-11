8dc774a0af5762c179da78e3f87af1caebf2c683
giordano.ch <jojowork4444@gmail.com>
2022-04-11 12:21:15 +0200

    add test; upd min-version cmake

--- test/point.cpp
+++ test/point.cpp
@@ -22,3 +22,45 @@ TEST(PtInTriangle, Board) {
         EXPECT_EQ(InTriangle(p1, p2, p3, p), true);
     }
 }
+
+TEST(PtInTriangle, Inside) {
+    Point p1(5, 5);
+    Point p2(-10, 10);
+    Point p3(5, 15);
+
+    Point<int, ll> ta[] = {
+            Point(4, 10),
+            Point(3, 10),
+            Point(2, 10),
+            Point(1, 10),
+            Point(0, 10),
+            Point(-2, 10),
+            Point(-5, 10),
+            Point(-9, 10),
+    };
+
+    for (const auto &p: ta) {
+        EXPECT_EQ(InTriangle(p1, p2, p3, p), true);
+    }
+}
+
+TEST(PtOutTriangle, out) {
+    Point p1(5, 5);
+    Point p2(10, 10);
+    Point p3(7, 12);
+
+    Point<int, ll> ta[] = {
+            Point(0, 1),
+            Point(4, 5),
+            Point(11, 10),
+            Point(4444, 4444),
+            Point(-1000000000, 1000000000),
+            Point(10, 9),
+            Point(7, 13),
+            Point(6, 13),
+    };
+
+    for (const auto &p: ta) {
+        EXPECT_EQ(InTriangle(p1, p2, p3, p), false);
+    }
+}
